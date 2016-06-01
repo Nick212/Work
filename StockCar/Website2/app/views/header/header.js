2e649d81175a69d5f02bb6d599fbf68cb990c76d
Nicolas <nick_carvalho29@hotmail.com>
2016-06-01 00:04:10 -0300

    adicionado novos projetos

---
+++ StockCar/Website2/app/views/header/header.js
@@ -0,0 +1,8 @@
+﻿(function (angular) {
+    var app = angular.module('StockCar');
+
+    var headerController = function ($scope) {
+        $scope.template = '/app/views/header/header.html';
+    }
+
+}(angular));
